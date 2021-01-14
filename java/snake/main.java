9f9afef27b2df0a34f3493527b7e87e1fbedda72
Okwuchukwu Somtochukwu Udeokpala <75144767+Somtohub@users.noreply.github.com>
2021-01-14 01:22:29 +0100

    The main class

---
+++ java/snake/main.java
@@ -0,0 +1,26 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Main 
+{
+	
+	public static void main(String[] args) 
+	{
+
+		JFrame myFrame = new JFrame();
+		
+		SnakePanel snakePanel = new SnakePanel();
+		myFrame.add(snakePanel);
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+// Set JFrame attributes		
+		myFrame.setTitle("SOMTO'S SNAKE");
+		myFrame.setResizable(false);
+		myFrame.pack();
+		
+		myFrame.setPreferredSize(new Dimension(SnakePanel.WIDTH, SnakePanel.HEIGHT));
+		myFrame.setLocationRelativeTo(null);
+		myFrame.setVisible(true);
+		
+	}
+}
